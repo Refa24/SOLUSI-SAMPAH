72f03abaee8be39ac41d1d91a18f4df85e741069
hestiyuliana <672022213@student.uksw.edu>
2024-11-13 22:31:11 +0700

    tentangkami.php

---
+++ tentangkami.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang Kami</title>
+    <link rel="stylesheet" href="tentangkami.css">
+</head>
+<body>
+    <header>
+        <div class="logo"></div>
+        <nav>
+            <a href="beranda.html">Beranda</a>
+            <a href="petaindex.php">Peta Lokasi</a>
+            <a href="riwayatlaporan.php">Riwayat Laporan</a>
+            <a href="edukasiindex.php">Edukasi</a>
+            <a href="hubungikami.php">Hubungi Kami</a>
+            <a href="tentangkami.php" class="active">Tentang Kami</a>
+        </nav>
+    </header>
+    <main>
+        <section class="about">
+            <h1>Tentang Kami</h1>
+            <p>Website ini dibangun untuk memfasilitasi masyarakat dalam melaporkan tumpukan sampah dengan mudah dan cepat.</p>
+        </section>
+    </main>
+</body>
+</html>
