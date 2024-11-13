f735f39cc49a47947a692b16ba2d6fc40dfc0144
hestiyuliana <672022213@student.uksw.edu>
2024-11-13 22:34:04 +0700

    peta.php

---
+++ peta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peta Lokasi</title>
+    <link rel="stylesheet" href="peta.css"/>
+
+</head>
+<body>
+    <div class="nav">
+        <div class="user-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
+        </div>
+        <a href="beranda.html">Beranda</a>
+        <a href="petaindex.php">Peta Lokasi</a>
+        <a href="riwayatlaporan.php">Riwayat Laporan</a>
+        <a href="edukasiindex.php">Edukasi</a>
+        <a href="hubungikami.php">Hubungi Kami</a>
+        <a href="tentangkami.php">Tentang Kami</a>
+        <button class="button">Laporkan Sampah</button>
+       
+    </div>
+    <div class="container">
+        <h2>Peta berikut menunjukkan lokasi sampah yang telah dilaporkan:</h2>
+        <div class="map-container">
+            Peta/Maps
+        </div>
+    </div>
+</body>
+</html>
